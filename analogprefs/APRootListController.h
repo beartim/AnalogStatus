12ebe5e7d61c40873be228822a2ee37a558c2701
beartim <45888736+beartim@users.noreply.github.com>
2026-09-24 16:05:47 +0800

    Use minimal iOS 9 Preferences declarations

--- analogprefs/APRootListController.h
+++ analogprefs/APRootListController.h
@@ -1,4 +1,11 @@
-#import <Preferences/PSListController.h>
+#import <UIKit/UIKit.h>
+
+// Minimal iOS 9-compatible declaration. We intentionally avoid importing the
+// current Theos Preferences headers because they use Objective-C syntax newer
+// than the legacy Linux iOS clang used for this armv7 + arm64 build.
+@interface PSListController : UIViewController
+- (NSArray *)loadSpecifiersFromPlistName:(NSString *)name target:(id)target;
+@end
 
 @interface APRootListController : PSListController
 - (void)respring;
